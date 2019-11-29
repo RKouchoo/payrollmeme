ac7574002184ffd21d836c85cce6a001bca56159
RKouchoo <r.kouchoo1@gmail.com>
2019-11-29 14:57:24 +1100

    merged to reduce complexity

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
+ 
 
 
+class TaxCalculator:
 
 
-def calcResTax(earnt):
-	afterTax = 0
+	def calcResTax(earnt):
+		afterTax = 0
 
-	return afterTax
-
-
-def calcHoldayTax(earnt):
-	afterTax = 0
-
-	return afterTax
+		return afterTax
 
 
-def loadPayRecords(file):
-	records = []
+	def calcHoldayTax(earnt):
+		afterTax = 0
 
-	return records
+		return afterTax
 
 
-def createNewPayRecord(id, hours, rates, visa, yearToDate):
-	rec = ""
+class csvImporter:
 
-	return rec
+
+	def loadPayRecords(file):
+		records = []
+
+		return records
+
+
+	def createNewPayRecord(id, hours, rates, visa, yearToDate):
+		rec = ""
+
+		return rec
+
+
+class PayRecord:
+	
+	global thisRecordID
+	global thisHours
+	global thisRates
+
+
+	def __init__(self, recordID, hours, rates):
+		thisRecordID = recordID
+		thisHours = hours
+		thisRates = rates
+
+
+	def getDetails():
+
+		return details
+
+
+	def getGross():
+
+		return gross
+
+
+	def getTax():
+
+		return tax
+
+
+	def getNet():
+
+		return net
+
+
