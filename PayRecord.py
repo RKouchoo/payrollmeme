4c4009d0c35082f7fdcbe5f97c66fc487eff6e36
RKouchoo <r.kouchoo1@gmail.com>
2019-11-29 14:29:56 +1100

    basic struct

---
+++ PayRecord.py
@@ -0,0 +1,31 @@
+
+global recordID
+global hours
+global rates
+
+
+def __init__(self, recordID, hours, rates):
+        
+
+
+
+def getDetails():
+
+	return details
+
+
+def getGross():
+
+	return gross
+
+
+def getTax():
+
+	return tax
+
+
+def getNet():
+
+	return net
+
+
---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+def calcResTax(earnt):
+	afterTax = 0
+
+	return afterTax
+
+
+def calcHoldayTax(earnt):
+	afterTax = 0
+
+	return afterTax
+
+
+def loadPayRecords(file):
+	records = []
+
+	return records
+
+
+def createNewPayRecord(id, hours, rates, visa, yearToDate):
+	rec = ""
+
+	return rec
