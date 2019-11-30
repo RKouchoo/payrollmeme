bef08bc9093b90d4b9415af8dbbd679d2c4d6b56
RKouchoo <r.kouchoo1@gmail.com>
2019-11-30 16:29:02 +1100

    added rations

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
- 
+# tax calculator app
+
+import csv
+
+# resident ratios
+# stored as arrays for use
+# -100 means 64 bit limit or 1.0E+99
+# [minCap, maxCap, ratePercent, neg]
+
+residentBracket1 = [-1, 72, 0.19, 0.19]
+residentBracket2 = [72, 361, 0.2342, 3.213]
+residentBracket3 = [361, 932, 0.3477, 44.2476]
+residentBracket4 = [932, 1380, 0.345, 41.7311]
+residentBracket5 = [1380, 3111, 0.39, 103.857]
+residentBracket6 = [3111, -100, 0.47, 352.78888]
+
+
+# holiday ratios
+# -100 means 64 bit limit or 1.0E+99
+# [minCap, maxCap, ratePercent]
+
+holidayBracket1 = [-1, 37000, 0.15]
+holidayBracket2 = [37000, 90000, 0.32]
+holidayBracket3 = [90000, 180000, 0.37]
+holidayBracket4 = [180000, -100, 0.45]
 
 
 class TaxCalculator:
-
 
 	def calcResTax(earnt):
 		afterTax = 0
@@ -62,5 +85,8 @@ class PayRecord:
 	def getNet():
 
 		return net
+
+
+
 
 
